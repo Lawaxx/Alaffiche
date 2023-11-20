3a412e9fa2f7e13b143ab4d3c2ea38bb50258e24
Aurelien WAXIN <aurelien.waxin@hotmail.fr>
2023-11-20 18:55:23 +0100

    Initial Commit

---
+++ FilmAPITest/FilmAPITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmAPITestApp.swift
+//  FilmAPITest
+//
+//  Created by Aurelien Waxin on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FilmAPITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
